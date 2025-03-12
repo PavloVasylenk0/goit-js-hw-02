8a504844900e0a92a084939dbcd1deecfed20225
Pavlo M <pavlovasilenko9@gmail.com>
2025-03-12 16:26:06 +0200

    js-hw-2

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+function checkForSpam(message) {
+  let lowCasemessage = message.toLowerCase();
+  return lowCasemessage.includes("spam") || lowCasemessage.includes("sale");
+}
+
+console.log(checkForSpam("Latest technology news"));
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!"));
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message"));
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!"));
+console.log(checkForSpam("[SPAM] How to earn fast money?"));
